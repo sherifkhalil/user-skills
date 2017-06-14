a941a35091b0b4af8112f817cf081b84d94643de
sherifkhalil <eng_sherifkhalil@hotmail.com>
2017-06-14 20:30:55 +0200

    adding the two controller methods spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+    describe 'GET #index' do
+        
+        before { get :index }
+
+        it { should respond_with(:ok) }
+    end
+
+    describe 'GET #show' do
+        
+        before { get :show, id: create(:user).id }
+
+        it { should respond_with(:ok) }
+    end
+
 end
