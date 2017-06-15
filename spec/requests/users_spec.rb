07be79c0d1cbc4cbbd4ee2313c55a4fce419fb79
sherifkhalil <eng_sherifkhalil@hotmail.com>
2017-06-15 04:13:21 +0200

    test that the index and show responses includes users associations

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,27 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   # initialize test data 
   let!(:users) { create_list(:user, 10) }
-  let(:user_id) { users.first.id }
+  let!(:skill) { create(:skill) }
+  let!(:category) { create(:category) }
+  let(:user) { users.first }
+  let(:user_id) { user.id }
+
+  before { 
+    skill.categories << category
+    skill.save
+    user.skills << skill
+    user.save
+ }
 
   describe 'GET /users' do
     before { get '/users' }
 
-    it 'returns users' do
+    it 'returns users and their skills and categories' do
       #`json` is a custom helper to parse JSON responses
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
+      expect(json.first['skills'].first['name']).to eq(skill.name)
+      expect(json.first['skills'].first['categories'].first['name']).to eq(category.name)
     end
 
     it 'returns status code 200' do
@@ -23,9 +35,11 @@ RSpec.describe 'Users API', type: :request do
     before { get "/users/#{user_id}" }
 
     context 'when the record exists' do
-      it 'returns the user' do
+      it 'returns the user and its skills and categories' do
         expect(json).not_to be_empty
-        expect(json['id']).to eq(user_id)
+        expect(json['id']).to eq(user.id)
+        expect(json['skills'].first['name']).to eq(skill.name)
+        expect(json['skills'].first['categories'].first['name']).to eq(category.name)
       end
 
       it 'returns status code 200' do
